f59fb27e142088460eb9f93b79afe33536004449
Davud <davud.aliyev2007@gmail.com>
2024-01-23 14:12:45 +0400

    Add project files.

---
+++ Homework 20/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_20
+{
+    internal class Student
+    {
+        public string Fullname;
+        public string GroupNo;
+        public int Age;
+    }
+}
